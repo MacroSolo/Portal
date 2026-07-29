d9e93f169ff0385ae750fc710698589350599980
Sydorenko.A <Sydorenko.A@buchi.com>
2026-07-29 18:30:42 +0200

    Colormap

--- main.py
+++ main.py
@@ -132,15 +132,15 @@ if __name__ == "__main__":
             # normalize to 0-255
             frame = cv2.normalize(frame, None, 0, 255, cv2.NORM_MINMAX)
 
+            colored_frame = cv2.applyColorMap(frame, cv2.COLORMAP_BONE)
             if mode == 0:
-                colored_frame = cv2.applyColorMap(frame, cv2.COLORMAP_BONE)
                 colored_frame[frame <= 15] = (255, 0, 0)
             elif mode == 1:
                 colored_frame = cv2.applyColorMap(frame, cv2.COLORMAP_JET)
-                colored_frame[frame <= 15] = (0, 255, 0)
+                colored_frame[frame <= 25] = (255, 0, 0)
             else:
                 colored_frame = cv2.applyColorMap(frame, cv2.COLORMAP_WINTER)
-                colored_frame[frame <= 15] = (0, 0, 255)
+                colored_frame[frame <= 35] = (255, 0, 0)
 
             frame = colored_frame
 
