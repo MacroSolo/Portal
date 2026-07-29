36ab14a891e6f6c3f222303085829ff5787dd419
Sydorenko.A <Sydorenko.A@buchi.com>
2026-07-29 14:26:41 +0200

    autogit

--- tools/imx477.py
+++ tools/imx477.py
@@ -23,7 +23,7 @@ def camera_loop():
         "AeEnable": False,
         "AwbEnable": False,
         "ExposureTime": 10_000,     # Exposure in microseconds
-        "AnalogueGain": 8.0,        # Fixed analog gain
+        "AnalogueGain": 5.0,        # Fixed analog gain
         "ColourGains": (1.5, 1.5)   # Fixed red/blue white balance multipliers
     })
 
