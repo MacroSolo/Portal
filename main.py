7636166cc0282e18ec8e83e4922aefcd7848ad7f
Sydorenko.A <Sydorenko.A@buchi.com>
2026-07-30 14:50:51 +0200

    New assembly

--- main.py
+++ main.py
@@ -113,7 +113,7 @@ if __name__ == "__main__":
         frame = frames[-1] if frames else None
 
         if frame is not None:
-            frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+            frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
 
             cpu_temo = 0.9 * cpu_temp + 0.1 * get_cpu_temperature()
 
