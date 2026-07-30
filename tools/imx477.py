415e294274dec6c37ff26a9b37b5ae9f5dbd3799
Sydorenko.A <Sydorenko.A@buchi.com>
2026-07-30 16:40:27 +0200

    Exposure

--- tools/imx477.py
+++ tools/imx477.py
@@ -26,7 +26,7 @@ def camera_loop():
     picam0.set_controls({
         "AeEnable": False,
         "AwbEnable": False,
-        "ExposureTime": 1024,     # Exposure in microseconds
+        "ExposureTime": 2048,     # Exposure in microseconds
         "AnalogueGain": 8,        # Fixed analog gain
         "ColourGains": (1.5, 1.5)   # Fixed red/blue white balance multipliers
     })
