1611770461db8caf2e30452e310b334332e0f91f
Sydorenko.A <Sydorenko.A@buchi.com>
2026-08-07 12:25:12 +0200

    Motor

--- tools/motor_driver.py
+++ tools/motor_driver.py
@@ -1,5 +1,5 @@
 from gpiozero import OutputDevice
-
+import time
 
 MOTOR_A = [OutputDevice(9), OutputDevice(11)]
 MOTOR_B = [OutputDevice(17), OutputDevice(27)]
@@ -16,10 +16,23 @@ def motor(ch, state):
         motor[0].value = 0
         motor[1].value = 0
 
+def switch_filter(ch, direction, pulse_ms=100):
+    """
+    ch: 0 or 1 - channel
+    direction: 1 - , 2 -
+    pulse_ms: pulse duration in milliseconds
+    """
+    motor(ch, direction)
+    time.sleep(pulse_ms / 1000)
+    motor(ch, 0)
+
 
 if __name__ == "__main__":
 
     for i in range(10):
+
         ch = int(input("Select motor channel (0 for A, 1 for B): "))
         state = int(input("Select motor state (0 for off, 1 for forward, 2 for backward): "))
-        motor(ch, state)
+        #motor(ch, state)
+        switch_filter(ch, state, pulse_ms=100)
+
