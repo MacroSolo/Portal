6715fb3f712b5e4fcbdb0a6d7bf3c9ce1f40d1f3
Sydorenko.A <Sydorenko.A@buchi.com>
2026-08-17 11:06:17 +0200

    COLORMAP

--- main.py
+++ main.py
@@ -139,7 +139,7 @@ def rank_normalize_image(img: np.ndarray) -> np.ndarray:
 
 
 if __name__ == "__main__":
-    lut_colormap = get_mpl_lut("gist_earth")
+    lut_colormap = get_mpl_lut("gnuplot")
 
     pin = OutputDevice(25, initial_value=False)
 
