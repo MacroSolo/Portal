dfd76a45847416db1ef7edeb64f67e93da6f6ae8
Sydorenko.A <Sydorenko.A@buchi.com>
2026-08-18 16:50:39 +0200

    Camera

--- tools/global_vars.py
+++ tools/global_vars.py
@@ -1,5 +1,11 @@
 global_state = {
     "RECORDING": False,
     "last_mqtt_message": None,
+    "camera": {
+        "exposure": None,
+        "gain": None,
+        "fps": None
+            },
+
         }
 
